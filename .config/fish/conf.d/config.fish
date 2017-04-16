b3e3207e7bfe68db26a48754836183c2c9a3b265
HIDEKUNI Kajita <hide.nba@gmail.com>
2017-04-16 12:55:54 +0900

    migrate sieera

---
+++ .config/fish/conf.d/config.fish
@@ -0,0 +1,17 @@
+rbenv init - | source
+
+balias rr "rm -rf"
+balias rm "rm -i"
+balias cp "cp -i"
+balias mv "mv -i"
+balias g "git"
+balias b "bundle"
+balias bi "bundle install"
+
+set -x EDITOR '/Applications/Emacs.app/Contents/MacOS/bin/emacsclient -t'
+
+function fish_user_key_bindings
+    bind \cs peco_select_ghq_repository
+    bind \cr peco_select_history
+    bind \cx\ck peco_kill
+end
